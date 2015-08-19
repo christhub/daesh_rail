4185283793879d419b0e42d5bbd41ab0fa2dfeb0
christian lei & Travis Johnston <student@epicodus.com>
2015-08-19 11:26:45 -0700

    add more methods to the train class

--- lib/trains.rb
+++ lib/trains.rb
@@ -1,8 +1,27 @@
 class Train
   attr_reader(:name, :id)
 
-define_method(:initialize) do |attributes|
-  @name = attributes.fetch(:name)
-  @id = attributes.fetch(:id)
-end
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  define_singleton_method(:all) do
+    database_results = DB.exec("SELECT * FROM trains;")
+    trains = []
+    database_results.each() do |train|
+      name = train.fetch('name')
+      id = train.fetch('id')
+        trains << Train.new(:name => name, :id => id.to_i)
+    end
+    trains
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO trains (name, id) VALUES ('#{name}', #{id})")
+  end
+
+  define_method(:==) do |other_train|
+    (self.name && self.id) == (other_train.name && other_train.id)
+  end
 end
--- spec/trains_spec.rb
+++ spec/trains_spec.rb
@@ -3,12 +3,39 @@ require('spec_helper')
 describe(Train) do
 
    before do
-     @new_train = Train.new(:name => "abu-bawr al-bagdadhi", :id => 1)
+     @new_train = Train.new(:name => "abu bakr al-baghdadhi", :id => 1)
    end
 
   describe('#id') do
     it('will return the id') do
       expect(@new_train.id).to(eq(1))
+    end
+  end
+
+  describe('#name') do
+    it('will return the name') do
+      expect(@new_train.name).to(eq("abu bakr al-baghdadhi"))
+    end
+  end
+
+  describe('.all') do
+    it('will initiate an empty array') do
+      expect(Train.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('will save the object into the database') do
+      @new_train.save
+      expect(Train.all).to(eq([@new_train]))
+    end
+  end
+
+  describe('#==') do
+    it("will compare two objects and see if they're the same") do
+      train1 = Train.new(:name => "abu bakr al-baghdadhi", :id => 1)
+      train2 = Train.new(:name => "abu bakr al-baghdadhi", :id => 1)
+      expect(train1).to(eq(train2))
     end
   end
 end
